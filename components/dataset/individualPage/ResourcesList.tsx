153103116591a01c8232443f081d1bf5197098e8
Demenech <joaommdemenech@gmail.com>
2026-01-20 12:56:22 -0300

    feat(#9): tweak Download button label to Access when resource is a external URL

--- components/dataset/individualPage/ResourcesList.tsx
+++ components/dataset/individualPage/ResourcesList.tsx
@@ -14,52 +14,61 @@ export default function ResourcesList({
   orgName,
   datasetName,
 }: ResourcesListProps) {
-  const {t} = useTranslation("common");
+  const { t } = useTranslation("common");
+
   return (
     <div className="py-8 w-full max-h-[600px] flex flex-col gap-4 ">
-      {resources.map((resource: Resource) => (
-        <div
-          key={resource.id}
-          className="flex flex-col md:flex-row  md:justify-between w-full gap-4  py-3 md:items-center px-2"
-        >
-          <article className="grid grid-cols-1 sm:grid-cols-6 gap-x-2 grow ">
-            <div className="col-span-5 place-content-start flex flex-col gap-0">
-              <h4 className=" md:m-0 font-semibold text-lg text-zinc-900 leading-tight line-clamp-3 pr-5">
-                {resource.name || "No title"}
-              </h4>
-              <p className="text-sm font-normal text-stone-500 line-clamp-4">
-                {resource.description || "No description"}
-              </p>
-              <div className="mt-2">
-                <ResourcesBadges resources={[resource]} />
+      {resources.map((resource: Resource) => {
+        const buttonLabel =
+          (["HTML", "Link"].includes(resource.format) || !resource.format) &&
+          !resource.url.startsWith("https://blob.datopian")
+            ? t("access")
+            : t("download");
+
+        return (
+          <div
+            key={resource.id}
+            className="flex flex-col md:flex-row  md:justify-between w-full gap-4  py-3 md:items-center px-2"
+          >
+            <article className="grid grid-cols-1 sm:grid-cols-6 gap-x-2 grow ">
+              <div className="col-span-5 place-content-start flex flex-col gap-0">
+                <h4 className=" md:m-0 font-semibold text-lg text-zinc-900 leading-tight line-clamp-3 pr-5">
+                  {resource.name || "No title"}
+                </h4>
+                <p className="text-sm font-normal text-stone-500 line-clamp-4">
+                  {resource.description || "No description"}
+                </p>
+                <div className="mt-2">
+                  <ResourcesBadges resources={[resource]} />
+                </div>
               </div>
+            </article>
+            <div className="flex  gap-2 justify-start pt-2 sm:pt-0">
+              {(["csv", "pdf", "xlsx", "xls", "geojson"].includes(
+                resource.format.toLowerCase()
+              ) ||
+                resource?.iframe) && (
+                <Link
+                  href={`/@${orgName}/${datasetName}/r/${resource.id}`}
+                  className="px-2 py-1 border  h-fit shadow hover:shadow-lg transition-all text-sm  text-center text-dark rounded font-roboto font-bold border-accent-50 hover:border-accent-100 hover:bg-accent-100  duration-150 flex items-center justify-center gap-1"
+                >
+                  <RiEyeLine width={18} className="min-w-[18px]" />
+                  <span>{t("preview")}</span>
+                </Link>
+              )}
+              {resource.url && (
+                <Link
+                  href={resource.url}
+                  className="bg-accent px-2 py-1 h-fit shadow hover:shadow-lg transition-all text-sm  text-center text-white rounded font-roboto font-bold hover:bg-darkaccent hover:text-white duration-150 flex items-center justify-center gap-1"
+                >
+                  <RiDownload2Fill width={18} className="min-w-[18px]" />
+                  <span>{buttonLabel}</span>
+                </Link>
+              )}
             </div>
-          </article>
-          <div className="flex  gap-2 justify-start pt-2 sm:pt-0">
-            {(["csv", "pdf", "xlsx", "xls", "geojson"].includes(
-              resource.format.toLowerCase()
-            ) ||
-              resource?.iframe) && (
-              <Link
-                href={`/@${orgName}/${datasetName}/r/${resource.id}`}
-                className="px-2 py-1 border  h-fit shadow hover:shadow-lg transition-all text-sm  text-center text-dark rounded font-roboto font-bold border-accent-50 hover:border-accent-100 hover:bg-accent-100  duration-150 flex items-center justify-center gap-1"
-              >
-                <RiEyeLine width={18} className="min-w-[18px]"/>
-                <span>{t("preview")}</span>
-              </Link>
-            )}
-            {resource.url && (
-              <Link
-                href={resource.url}
-                className="bg-accent px-2 py-1 h-fit shadow hover:shadow-lg transition-all text-sm  text-center text-white rounded font-roboto font-bold hover:bg-darkaccent hover:text-white duration-150 flex items-center justify-center gap-1"
-              >
-                <RiDownload2Fill width={18} className="min-w-[18px]"/>
-                <span>{t("download")}</span>
-              </Link>
-            )}
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
